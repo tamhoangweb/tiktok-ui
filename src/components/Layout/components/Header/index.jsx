ad43eae39f2837fb2bf07eb299986758771e418b
tamhoangweb <tamhoangweb@gmail.com>
2022-08-07 09:30:42 +0700

    Header: logo SVG + Input focus-within + Input ~Search btn

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+import { images } from '~/assets/images';
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img className={cx('logo')} src={images.logo} alt="logo tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>Login</div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
